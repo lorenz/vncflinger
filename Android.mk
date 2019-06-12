52920978558d972a4ebd40f9a84b463489baef72
Lorenz Brun <lorenz@dolansoft.org>
2019-06-12 20:14:58 +0000

    A bunch of SELinux fixes
    Change-Id: I3a6d0fc3c5856f8e25a8f92a1137231b243af36d

--- Android.mk
+++ Android.mk
@@ -32,7 +32,7 @@ LOCAL_STATIC_LIBRARIES += \
     libvncserver
 
 LOCAL_CFLAGS := -DVNCFLINGER_VERSION="0.1"
-LOCAL_CFLAGS += -Ofast -Werror -std=c++11
+LOCAL_CFLAGS += -Ofast -Werror -std=c++14
 
 #LOCAL_CFLAGS += -DLOG_NDEBUG=0
 #LOCAL_CXX := /usr/bin/include-what-you-use
